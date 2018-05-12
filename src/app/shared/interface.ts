f51459c6e3c27ceec5788ccaf93ae84ea5205b94
VinayV9 <vinayveeren@gmail.com>
2018-05-12 23:50:49 +0530

    adding more style

--- src/app/shared/interface.ts
+++ src/app/shared/interface.ts
@@ -21,8 +21,14 @@ export const questions : question[]=
     audioUrl:'URL'
   },
   {
-    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
-    choices: [{option: 'A', answer: false }, {option: 'B', answer: false }, {option: 'C', answer: false }, {option: 'D', answer: false }],
+    label:'What is the current version of angular?',
+    choices: [{option: '1', answer: false }, {option: '2', answer: false }, {option: '6', answer: false }, {option: '7', answer: false }],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'How many colors are their in rainbow?',
+    choices: [{option: '3', answer: false }, {option: '7', answer: false }, {option: '1', answer: false }, {option: '4', answer: false }],
     answer:'ANSWER',
     audioUrl:'URL'
   },
@@ -44,17 +50,6 @@ export const questions : question[]=
     answer:'ANSWER',
     audioUrl:'URL'
   },
-  {
-    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
-    choices: [{option: 'A', answer: false }, {option: 'B', answer: false }, {option: 'C', answer: false }, {option: 'D', answer: false }],
-    answer:'ANSWER',
-    audioUrl:'URL'
-  },
-  {
-    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
-    choices: [{option: 'A', answer: false }, {option: 'B', answer: false }, {option: 'C', answer: false }, {option: 'D', answer: false }],
-    answer:'ANSWER',
-    audioUrl:'URL'
-  }
+  
 ];
   
