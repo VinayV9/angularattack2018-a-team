46a4e30cc4d3e6998c3b82b24754e93cca2d1d56
VinayV9 <vinayveeren@gmail.com>
2018-05-12 19:53:23 +0530

    question model updated

--- src/app/shared/interface.ts
+++ src/app/shared/interface.ts
@@ -1,26 +1,60 @@
 export class  question {
    public   label:string;
-   public  A:string;
-   public  B:string;
-   public  C:string;
-   public  D: string;
+   public choices: choices[];
    public  answer: string;
    public  audioUrl: string;    
-}
+};
 
+export class choices{
+    public choice: string;
+};
 
 
 export const questions = 
 
-[{label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'2Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'3Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'4Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'5Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'6Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'7Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'8Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'9Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'},
- {label:'10Which of these U.S Presidents appeared on the television series "laugh-In"?',A:'A',B:'B',C:'C',D:'D',answer:'ANSWER',audioUrl:'URL'}
+[
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  },
+  {
+    label:'Which of these U.S Presidents appeared on the television series "laugh-In"?',
+    choices: [{choice: 'A'},{choice: 'B'},{choice: 'C'},{choice: 'D'}],
+    answer:'ANSWER',
+    audioUrl:'URL'
+  }
+ 
 ];
   
