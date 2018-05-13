f9d25761a37dc5ad35987d4aefb45d87c2acfc99
VinayV9 <vinayveeren@gmail.com>
2018-05-13 14:08:23 +0530

    home component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import {QuestionComponent} from './components/question/question.component';
 import {CountdownTimerComponent} from './components/countdown-timer/countdown-timer.component';
 import {QuestionGoogleFormComponent} from './components/question-google-form/question-google-form.component';
 import {AudioQuestionComponent} from './components/audio-question/audio-question.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  // { path: '', component: AudioQuestionComponent },
-  { path: 'question', component: QuestionComponent },
-  { path: 'audio-question', component: AudioQuestionComponent },
-  { path: '', component: CountdownTimerComponent },
+  { path: '', component: HomeComponent },
+  { path: 'quiz', component: QuestionComponent },
+  { path: 'audio-quiz', component: AudioQuestionComponent },
   { path: 'question-form', component: QuestionGoogleFormComponent },
   { path: '**', component: QuestionComponent }
 ];
